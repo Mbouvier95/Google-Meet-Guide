3e5ab150d329f53335b742c5477b48ec08f6ef86
Mbouvier95 <145860996+Mbouvier95@users.noreply.github.com>
2023-11-05 16:08:03 -0600

    initial project commit

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Google",p:["p1","p4"]},{w:"Meet:",p:["p1"]},{w:"A",p:["p1","p4"]},{w:"Virtual",p:["p1"]},{w:"Collaboration",p:["p1"]},{w:"How",p:["p2","p3"]},{w:"to",p:["p2","p3"]},{w:"Get",p:["p2"]},{w:"Started",p:["p2"]},{w:"Send",p:["p3"]},{w:"Messages",p:["p3"]},{w:"Presenting",p:["p4"]},{w:"During",p:["p4"]},{w:"Meeting",p:["p4"]},{w:"Speak",p:["p5"]},{w:"Up",p:["p5"]},{w:"and",p:["p5"]},{w:"Ask",p:["p5"]},{w:"Questions",p:["p5"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8, 9, 10, 11];
+ph["p3"]=[8, 9, 12, 13];
+ph["p4"]=[14, 15, 5, 3, 16];
+ph["p5"]=[17, 18, 19, 20, 21];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
